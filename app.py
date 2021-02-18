deeb06af0ae51eb639f3a2121a8fba8846cb10c5
Narinder <narinderv@gmail.com>
2021-02-18 22:14:05 +0530

    Added run.py for heroku

--- app.py
+++ app.py
@@ -30,9 +30,4 @@ api.add_resource(Store, '/store/<string:name>')
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
-
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
